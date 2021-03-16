fdc8a3b19bb4ec5aeca7dc41f7df66533799c068
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-03-16 07:50:25 +0300

    Update FileUpload.js

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState,useEffect } from 'react';
 import Message from './Message';
 import Progress from './Progress';
 import axios from 'axios';
@@ -9,41 +9,44 @@ const FileUpload = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [message, setMessage] = useState('');
   const [uploadPercentage, setUploadPercentage] = useState(0);
+  const[ uploadProgress,setProgress]=useState(0);
+  const[zip,setZip]=useState([]);
 
+useEffect(() => {
+    downloadZip();
+  },[]);
    const handleDrop = acceptedFiles =>
     setFilenames(acceptedFiles.map(file => file.name));
-
     const onChange = e => {
       setFile(e.target.files);
     };
-    //  console.log(files)
+   
     const onSubmit = async e => {
       e.preventDefault();
       const formData = new FormData();
       for(var x = 0; x<files.length; x++) {
-        formData.append('file', files[x])
+        formData.append('file', files[x]);  
     }
-    //  console.log(formData.get('file'));
     try {
       const res = await axios.post("/multiple", formData,{
-        headers: {
-          'Content-Type': 'multipart/form-data'
+         headers: {
+          'Content-Type': 'multipart/form-data',
         },
+      
         onUploadProgress: progressEvent => {
           setUploadPercentage(
             parseInt(
               Math.round((progressEvent.loaded * 100) / progressEvent.total)
             )
           );
+
           // Clear percentage
           setTimeout(() => setUploadPercentage(0), 10000);
         }
       });
-      // console.log(res.data)
-      // console.log(files)
-      //  const { fileName, filePath,preview,thumbnail,original } = res.data;
-      // files({ fileName, filePath,preview,thumbnail,original });
-      setUploadedFiles(res.data)
+      
+      setUploadedFiles(res.data.results)
+      setZip(res.data.zip)
       setMessage('File Uploaded');
     } catch (err) {
       if (err) {
@@ -52,27 +55,74 @@ const FileUpload = () => {
         setMessage(err.response.data.msg);
       }
     }
-  };
+  }
 
+    const downloadZip = async ()  =>{
+       axios({
+        url: zip,
+        method:'GET',
+        responseType: "blob",
+        onDownloadProgress: progressEvent => {
+          const percentage = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setProgress(percentage);
+          if (percentage === 100) {
+            setTimeout(() => setProgress(0), 10000);
+          }
+        }
+        })
+        .then(res => {
+          const Fileurl = window.URL.createObjectURL(new Blob([res.data]));
+          const filelink = document.createElement("a");
+          filelink.href = Fileurl;
+          var filename = zip.substring(zip.lastIndexOf('/')+1);
+          filename.toString();
+          filelink.setAttribute("download", filename); 
+          document.body.appendChild(filelink);
+          filelink.click();
+          
+        })
+        .catch(error => {
+          setMessage('There was an error downloading the image');
+          return  [];
+        });
+    }
   return (
     <Fragment>
+       
       {message ? <Message msg={message} /> : null}
       <form  onSubmit={onSubmit} >
         <div>
-        <Dropzone onDrop={handleDrop}>
+        <Dropzone onDrop={handleDrop} > 
         {({ getRootProps, getInputProps }) => (
+          
           <div {...getRootProps({ className: "dropzone" })}>
             <input {...getInputProps()} 
-            onChange={onChange}
-            /> 
-            <p>Drag'n'drop files, or click to select file</p>
-            {/* {filenames}  */}
+           onChange={onChange}
            
+            /> 
+           
+            <p>Drag'n'drop files, or click to select file</p>
+
+            {filenames} 
+            
           </div>
           
-        )}
-        
+        )}  
       </Dropzone>
+      <div>
+          <h1 className="text-center">
+              Compress Files to ZIP File
+          </h1>
+         <div>
+         <Progress percentage={uploadProgress} />
+                <button className="btn btn-block btn-danger" onClick={downloadZip}> 
+                 Download ZIP File
+                </button>
+                </div> 
+            </div>  
+      
       <div>
         <ul>
           {filenames.map(fileName => (
@@ -81,35 +131,41 @@ const FileUpload = () => {
         </ul>
       </div>
       </div>
-        <Progress percentage={uploadPercentage} />
+<Progress percentage={uploadPercentage} />
+
         <input
           type='submit'
           value='Upload'
           className='btn btn-primary btn-block mt-4' 
+           
         />
-      </form>     
+      </form> 
+     
 <div className="image">
-<table name="image">
+<table className="image">
 <tr>
   <th>Original</th>
   <th>Preview</th>
   <th>Thumbnail</th>
+ 
 </tr>
 <tbody>
 {uploadedFiles.map(file =>(
   <tr>
-      <td><img style={{ width: '50%' }} src={file.original} alt='' /></td>
+      <td><img style={{ width: '50%' }} src={file.filePath} alt='' /></td>
       <td><img style={{ width: '50%' }} src={file.preview} alt='' /></td>
-      <td><img style={{ width: '50%' }} src={file.thumbail} alt='' /></td>
+      <td><img style={{ width: '50%' }} src={file.thumbnail} alt='' /></td>        
     </tr>
   ))}
   </tbody>
+  
  </table>
+ 
 </div> 
+
     </Fragment>
     
   );
-  
 };
-export default FileUpload;
 
+export default FileUpload;

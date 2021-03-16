be5bf08720142523d63d296d6bf86bc03c0a15df
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-03-16 07:51:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 var express=require('express');
 var path = require('path');
 var app =express();
 var multer  = require('multer');
 var cors = require('cors');
+var AdmZip = require('adm-zip');
 const sharp = require('sharp');
 const fs = require("fs");
 app.use(express.json());
@@ -23,62 +23,86 @@ var storage = multer.diskStorage({
     // }
 });
 var upload = multer({ storage: storage });
-// var upload = multer({ storage: storage }).array('files');
 app.post('/upload', upload.single('file'), function (req, res, next) {
     const host = req.hostname;
-    // return res.json(req.file);
 const filePathOriginal = req.protocol + "://" + host + ':8000/uploads/' + req.file.filename;
 const filePathThumbnail = req.protocol + "://" + host + ':8000/uploads/thumbnail/' + req.file.filename;
 const filePathPreview = req.protocol + "://" + host + ':8000/uploads/preview/' + req.file.filename;
 
-sharp(__dirname + '/uploads/'+req.file.filename).resize(200,200) // TODO: use req.file.filename
+sharp(__dirname + '/uploads/'+req.file.filename).resize(200,200) 
 .jpeg({quality : 50}).toFile(__dirname  
-    + '/uploads/thumbnail/'+req.file.filename); // TODO use thumb folder and set name to req.file.filename
+    + '/uploads/thumbnail/'+req.file.filename); 
 sharp(__dirname + '/uploads/'+req.file.filename).resize(640,480) 
 .jpeg({quality : 80}).toFile(__dirname  
-    + '/uploads/preview/'+req.file.filename);  // TODO use preview folder and set name to req.file.filename
+    + '/uploads/preview/'+req.file.filename); 
     
-return res.json({ // TODO: return imgage that is  with preview and thumbnail
+return res.json({ 
     origianl: filePathOriginal,
     preview:filePathPreview,
     thumbnail:filePathThumbnail,
-     filePath: `uploads/${req.file.filename}`
+    
+    filePath: `uploads/${req.file.filename}`
 });
+
 })
 app.post('/multiple',upload.array('file',9),(req,res)=>{ 
-    let results = req.files.map(oneFile =>{
-        const host = req.hostname;   
+    const host = req.hostname;
+    var zip = new AdmZip();
+    
+    const zipname=Date.now()+'.zip';
+    
+const zippath=req.protocol + "://" + host + ':8000/uploads/zipped/' + zipname;
+const zippathoriginal=req.protocol + "://" + host + ':8000/uploads/zipped/origInal/' + zipname;
+const zippaththumbnail=req.protocol + "://" + host + ':8000/uploads/zipped/thumbnail/' + zipname;
+const zippathpreview=req.protocol + "://" + host + ':8000/uploads/zipped/preview/' + zipname;
+    let results = req.files.map(oneFile =>{  
+        var outputFilePath = req.protocol + "://" + host + ':8000/uploads/zipped/' + Date.now() + ".zip";
         const filePathOriginal = req.protocol + "://" + host + ':8000/uploads/' + oneFile.filename;
-        const filePathThumbnail = req.protocol + "://" + host + ':8000/uploads/thumbnail/' + oneFile.filename;
+        const filePathThumbnail =req.protocol + "://" + host + ':8000/uploads/thumbnail/' + oneFile.filename;
         const filePathPreview = req.protocol + "://" + host + ':8000/uploads/preview/' +oneFile.filename;
-         const filePathCompressed=req.protocol + "://" + host + ':8000/uploads/compressed/' +oneFile.filename;
         sharp(__dirname + '/uploads/'+oneFile.filename).resize(200,200)
         .jpeg({quality : 50}).toFile(__dirname  
             + '/uploads/thumbnail/'+oneFile.filename); 
         sharp(__dirname + '/uploads/'+oneFile.filename).resize(640,480) 
         .jpeg({quality : 80}).toFile(__dirname  
             + '/uploads/preview/'+oneFile.filename); 
-            sharp(__dirname + '/uploads/'+oneFile.filename).resize(200,200) 
-        .jpeg({chromaSubsampling:'4:4:4',quality:50}).toFile(__dirname  
-            + '/uploads/compressed/'+oneFile.filename); 
+            if (req.files) {
+                req.files.forEach((file) =>{   
+                zip.addLocalFile(file.path);
+                zip.addLocalFile(__dirname  
+                    + '/uploads/thumbnail/'+oneFile.filename); 
+                zip.addLocalFile(__dirname  
+                    + '/uploads/preview/'+oneFile.filename);
+                });
+            }        
             return {
                 origianl: filePathOriginal,
-    preview:filePathPreview,
-    thumbnail:filePathThumbnail,
-    compressed:filePathCompressed,
-    filePath: `uploads/${oneFile.filename}`
-     
+               preview:filePathPreview,
+               thumbnail:filePathThumbnail,
+              output:outputFilePath,
+           filePath: `uploads/${oneFile.filename}`
             }
-    })   
-return res.json(
-    results
+    })  
     
+        zip.writeZip(__dirname  
+            + '/uploads/zipped/'+zipname); 
+            zip.writeZip(__dirname  
+                + '/uploads/zipped/original/'+zipname); 
+            zip.writeZip(__dirname  
+                + '/uploads/zipped/preview/'+zipname); 
+                zip.writeZip(__dirname  
+                    + '/uploads/zipped/thumbnail/'+zipname); 
+                    zip.extractAllTo(__dirname  
+                        + '/uploads/extracts/', true);
+return res.json(
+    {results, zip:zippath ,original:zippathoriginal,preview:zippathpreview,thumbnail:zippaththumbnail}  
 );
-   //res.send(req.files)  
+   
 })
 
 app.listen(8000, function(){
   console.log("server is listening on port: 8000");
 });
+
 module.exports = app;
 
